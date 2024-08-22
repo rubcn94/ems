ec41642a65d936651d3e5b560aeabc4fd8217935
rubcn94 <126766745+rubcn94@users.noreply.github.com>
2024-08-22 17:59:22 +0200

    1

---
+++ ems/src/main/java/miproj/ems/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package miproj.ems.repository;
+
+import miproj.ems.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
---
+++ ems/src/main/java/miproj/ems/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package miproj.ems.repository;
+
+import miproj.ems.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
